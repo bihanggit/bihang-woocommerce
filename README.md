fe3bbddab706037916c6f82a57e44c74a58010e5
moxing <star2ko@gmail.com>
2016-04-08 18:05:54 +0800

    oklink->bihang

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-oklink-woocommerce
+bihang-woocommerce
 ====================
 
-Accept Bitcoin on your WooCommerce-powered website with Oklink.
+Accept Bitcoin on your WooCommerce-powered website with bihang.
 
 ## Installation
 
- Generate an API key at [https://oklink.com/apiKey/index.do](https://oklink.com/apiKey/index.do "oklink") If you don't have a Oklink account, sign up at [https://oklink.com](https://oklink.com "https://oklink.com").For more infomation on setting up payouts, see [https://oklink.com/open/api/rest_getStarted.do](https://oklink.com/open/api/rest_getStarted.do "https://oklink.com/open/api/rest_getStarted.do").
+ Generate an API key at [https://bihang.com/apiKey/index.do](https://bihang.com/apiKey/index.do "bihang") If you don't have a bihang account, sign up at [https://bihang.com](https://bihang.com "https://bihang.com").For more infomation on setting up payouts, see [https://bihang.com/open/api/rest_getStarted.do](https://bihang.com/open/api/rest_getStarted.do "https://bihang.com/open/api/rest_getStarted.do").
 
 To install the plugin:
 
-1. [Download](https://github.com/oklink/oklink-woocommerce/archive/master.zip) the plugin as a .zip file.
+1. [Download](https://github.com/bihang/bihang-woocommerce/archive/master.zip) the plugin as a .zip file.
 2. In your WordPress administration console, navigate to Plugins > Add New > Upload. 
 3. Upload the .zip file downloaded in step 1.
 4. Click 'Install Now' and then 'Activate Plugin.'
 5. Navigate to WooCommerce > Settings, and then click on the Checkout tab at the top of the screen.
-6. Click on Oklink.
+6. Click on bihang.
 7. Enter your API Credentials and click on 'Save changes'.
 
 
--- bihang-woocommerce.php
+++ bihang-woocommerce.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Plugin Name: oklink-woocommerce
- * Plugin URI: https://github.com/oklink/oklink-woocommerce
- * Description: Accept Bitcoin on your WooCommerce-powered website with Oklink.
+ * Plugin Name: bihang-woocommerce
+ * Plugin URI: https://github.com/bihang/bihang-woocommerce
+ * Description: Accept Bitcoin on your WooCommerce-powered website with bihang.
  * Version: 1.0.0
- * Author: Oklink Inc.
- * Author URI: https://oklink.com
+ * Author: bihang Inc.
+ * Author URI: https://bihang.com
  * License: MIT
- * Text Domain: oklink-woocommerce
+ * Text Domain: bihang-woocommerce
  */
 
-/*  Copyright 2014 Oklink Inc.
+/*  Copyright 2014 bihang Inc.
 
 MIT License
 
@@ -40,30 +40,30 @@ if (!defined('ABSPATH')) {
 
 if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
 
-	function oklink_woocommerce_init() {
+	function bihang_woocommerce_init() {
 
 		if (!class_exists('WC_Payment_Gateway'))
 			return;
 
 		/**
-		 * oklink Payment Gateway
+		 * bihang Payment Gateway
 		 *
-		 * Provides a oklink Payment Gateway.
+		 * Provides a bihang Payment Gateway.
 		 *
-		 * @class       WC_Gateway_Oklink
+		 * @class       WC_Gateway_bihang
 		 * @extends     WC_Payment_Gateway
 		 * @version     1.0.0
-		 * @author      Oklink Inc.
+		 * @author      bihang Inc.
 		 */
-		class WC_Gateway_Oklink extends WC_Payment_Gateway {
+		class WC_Gateway_Bihang extends WC_Payment_Gateway {
 			var $notify_url;
 
 			public function __construct() {
-				$this->id   = 'oklink';
-				$this->icon = WP_PLUGIN_URL . "/" . plugin_basename(dirname(__FILE__)) . '/oklink.png';
+				$this->id   = 'bihang';
+				$this->icon = WP_PLUGIN_URL . "/" . plugin_basename(dirname(__FILE__)) . '/bihang.png';
 
 				$this->has_fields        = false;
-				$this->order_button_text = __('Proceed to Oklink', 'oklink-woocommerce');
+				$this->order_button_text = __('Proceed to Bihang', 'bihang-woocommerce');
 				$this->notify_url        = $this->construct_notify_url();
 
 				$this->init_form_fields();
@@ -77,27 +77,27 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 					$this,
 					'process_admin_options'
 				));
-				add_action('woocommerce_receipt_oklink', array(
+				add_action('woocommerce_receipt_bihang', array(
 					$this,
 					'receipt_page'
 				));
 
 				// Payment listener/API hook
-				add_action('woocommerce_api_wc_gateway_oklink', array(
+				add_action('woocommerce_api_wc_gateway_bihang', array(
 					$this,
-					'check_oklink_callback'
+					'check_bihang_callback'
 				));
 			}
 
 			public function admin_options() {
-				echo '<h3>' . __('Oklink Payment Gateway', 'oklink-woocommerce') . '</h3>';
-				$oklink_account_email = get_option("oklink_account_email");
+				echo '<h3>' . __('bihang Payment Gateway', 'bihang-woocommerce') . '</h3>';
+				$bihang_account_email = get_option("bihang_account_email");
 			
-				$oklink_error_message = get_option("oklink_error_message");
-				if ($oklink_account_email != false) {
-					echo '<p>' . __('Successfully connected Oklink account', 'oklink-woocommerce') . " '$oklink_account_email'" . '</p>';
-				} elseif ($oklink_error_message != false) {
-					echo '<p>' . __('Could not validate API Key:', 'oklink-woocommerce') . " $oklink_error_message" . '</p>';
+				$bihang_error_message = get_option("bihang_error_message");
+				if ($bihang_account_email != false) {
+					echo '<p>' . __('Successfully connected bihang account', 'bihang-woocommerce') . " '$bihang_account_email'" . '</p>';
+				} elseif ($bihang_error_message != false) {
+					echo '<p>' . __('Could not validate API Key:', 'bihang-woocommerce') . " $bihang_error_message" . '</p>';
 				}
 				echo '<table class="form-table">';
 				$this->generate_settings_html();
@@ -108,33 +108,33 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 				if (!parent::process_admin_options())
 					return false;
 
-				require_once(plugin_dir_path(__FILE__) . 'lib' . DIRECTORY_SEPARATOR . 'Oklink.php');
+				require_once(plugin_dir_path(__FILE__) . 'lib' . DIRECTORY_SEPARATOR . 'Bihang.php');
 
 				$api_key    = $this->get_option('apiKey');
 				$api_secret = $this->get_option('apiSecret');
 
 				// Validate merchant API key
 				try {
-					$client = OKlink::withApiKey($api_key, $api_secret);
+					$client = Bihang::withApiKey($api_key, $api_secret);
 					$result = $client->userInfoUser();
-					update_option("oklink_account_email", $result->user->email);
-					update_option("oklink_error_message", false);
+					update_option("bihang_account_email", $result->user->email);
+					update_option("bihang_error_message", false);
 				}
 				catch (Exception $e) {
 					$error_message = $e->getMessage();
-					update_option("oklink_account_email", false);
-					update_option("oklink_error_message", $error_message);
+					update_option("bihang_account_email", false);
+					update_option("bihang_error_message", $error_message);
 					return;
 				}
 			}
 
 			function construct_notify_url() {
-				$callback_secret = get_option("oklink_callback_secret");
+				$callback_secret = get_option("bihang_callback_secret");
 				if ($callback_secret == false) {
 					$callback_secret = sha1(openssl_random_pseudo_bytes(20));
-					update_option("oklink_callback_secret", $callback_secret);
+					update_option("bihang_callback_secret", $callback_secret);
 				}
-				$notify_url = WC()->api_request_url('WC_Gateway_oklink');
+				$notify_url = WC()->api_request_url('WC_Gateway_bihang');
 				$notify_url = add_query_arg('callback_secret', $callback_secret, $notify_url);
 				return $notify_url;
 			}
@@ -142,33 +142,33 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 			function init_form_fields() {
 				$this->form_fields = array(
 					'enabled' => array(
-						'title' => __('Enable Oklink plugin', 'oklink-woocommerce'),
+						'title' => __('Enable bihang plugin', 'bihang-woocommerce'),
 						'type' => 'checkbox',
-						'label' => __('Show bitcoin as an option to customers during checkout?', 'oklink-woocommerce'),
+						'label' => __('Show bitcoin as an option to customers during checkout?', 'bihang-woocommerce'),
 						'default' => 'yes'
 					),
 					'title' => array(
 						'title' => __('Title', 'woocommerce'),
 						'type' => 'text',
 						'description' => __('This controls the title which the user sees during checkout.', 'woocommerce'),
-						'default' => __('Bitcoin', 'oklink-woocommerce')
+						'default' => __('Bitcoin', 'bihang-woocommerce')
 					),
 					'description' => array(
 						'title'       => __( 'Description', 'woocommerce' ),
 						'type'        => 'textarea',
 						'description' => __( 'This controls the description which the user sees during checkout.', 'woocommerce' ),
-						'default'     => __('Pay with bitcoin, a virtual currency.', 'oklink-woocommerce')
+						'default'     => __('Pay with bitcoin, a virtual currency.', 'bihang-woocommerce')
 											. " <a href='http://bitcoin.org/' target='_blank'>"
-											. __('What is bitcoin?', 'oklink-woocommerce')
+											. __('What is bitcoin?', 'bihang-woocommerce')
 											. "</a>"
 	             	),
 					'apiKey' => array(
-						'title' => __('API Key', 'oklink-woocommerce'),
+						'title' => __('API Key', 'bihang-woocommerce'),
 						'type' => 'text',
 						'description' => __('')
 					),
 					'apiSecret' => array(
-						'title' => __('API Secret', 'oklink-woocommerce'),
+						'title' => __('API Secret', 'bihang-woocommerce'),
 						'type' => 'password',
 						'description' => __('')
 					)
@@ -177,12 +177,12 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 
 			function process_payment($order_id) {
 
-				require_once(plugin_dir_path(__FILE__) . 'lib' . DIRECTORY_SEPARATOR . 'Oklink.php');
+				require_once(plugin_dir_path(__FILE__) . 'lib' . DIRECTORY_SEPARATOR . 'Bihang.php');
 				global $woocommerce;
 
 				$order = new WC_Order($order_id);
 
-				$success_url = add_query_arg('return_from_oklink', true, $this->get_return_url($order));
+				$success_url = add_query_arg('return_from_bihang', true, $this->get_return_url($order));
 
 				if( get_woocommerce_currency()=='USD' || get_woocommerce_currency()=='CNY'  ){
 					$params = array(
@@ -203,43 +203,43 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 					}
 
 					try {
-						$client   = Oklink::withApiKey($api_key, $api_secret);
+						$client   = Bihang::withApiKey($api_key, $api_secret);
 						$result   = $client->buttonsButton($params);
 					}
 					catch (Exception $e) {
-						$order->add_order_note(__('Error while processing oklink payment:', 'oklink-woocommerce') . ' ' . var_export($e, TRUE));
-						$woocommerce->add_error(__($e->getMessage(), 'oklink-woocommerce'));				
+						$order->add_order_note(__('Error while processing bihang payment:', 'bihang-woocommerce') . ' ' . var_export($e, TRUE));
+						$woocommerce->add_error(__($e->getMessage(), 'bihang-woocommerce'));				
 						// $woocommerce->add_error(__('Sorry, but there was an error processing your order. Please try again or try a different payment method.', 'coinbase-woocommerce'));
 						return;
 					}
 
 					return array(
 						'result'   => 'success',
-						'redirect' =>  OklinkBase::WEB_BASE."merchant/mPayOrderStemp1.do?buttonid=".$result->button->id,
+						'redirect' =>  BihangBase::WEB_BASE."merchant/mPayOrderStemp1.do?buttonid=".$result->button->id,
 					);			
 				}else{
-					$woocommerce->add_error(__('only support USD and CNY', 'oklink-woocommerce'));
+					$woocommerce->add_error(__('only support USD and CNY', 'bihang-woocommerce'));
 					return;
 				}
 
 
 			}
 
-			function check_oklink_callback() {
-				require_once(plugin_dir_path(__FILE__) . 'lib' . DIRECTORY_SEPARATOR . 'Oklink.php');
+			function check_bihang_callback() {
+				require_once(plugin_dir_path(__FILE__) . 'lib' . DIRECTORY_SEPARATOR . 'Bihang.php');
 				$api_key    = $this->get_option('apiKey');
 				$api_secret = $this->get_option('apiSecret');
-				$client   = Oklink::withApiKey($api_key, $api_secret);
+				$client   = Bihang::withApiKey($api_key, $api_secret);
 
 				if ($client->checkCallback()) {
 					$post_body = json_decode(file_get_contents("php://input"));
 					if (isset($post_body)) {
-						$oklink_order = $post_body;
-						$order_id     = $oklink_order->custom;
+						$bihang_order = $post_body;
+						$order_id     = $bihang_order->custom;
 						$order        = new WC_Order($order_id);
 					} else {
 						header("HTTP/1.1 400 Bad Request");
-						exit("Unrecognized Oklink Callback");
+						exit("Unrecognized bihang Callback");
 					}
 				} else {
 					header("HTTP/1.1 401 Not Authorized");
@@ -247,12 +247,12 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 				}
 
 				header('HTTP/1.1 200 OK');
-				update_post_meta($order->id, __('Oklink Order ID', 'oklink-woocommerce'), wc_clean($oklink_order->id));
-				// if (isset($oklink_order->customer) && isset($oklink_order->customer->email)) {
-				// 	update_post_meta($order->id, __('Oklink Account of Payer', 'oklink-woocommerce'), wc_clean($oklink_order->customer->email));
+				update_post_meta($order->id, __('Bihang Order ID', 'bihang-woocommerce'), wc_clean($bihang_order->id));
+				// if (isset($bihang_order->customer) && isset($bihang_order->customer->email)) {
+				// 	update_post_meta($order->id, __('bihang Account of Payer', 'bihang-woocommerce'), wc_clean($bihang_order->customer->email));
 				// }
 
-				switch (strtolower($oklink_order->status)) {
+				switch (strtolower($bihang_order->status)) {
 
 					case 'completed':
 						// Check order not already completed
@@ -260,13 +260,13 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 							exit;
 						}
 
-						$order->add_order_note(__('Oklink payment completed', 'oklink-woocommerce'));
+						$order->add_order_note(__('Bihang payment completed', 'bihang-woocommerce'));
 						$order->payment_complete();
 
 						break;
 					case 'canceled':
 
-						$order->update_status('failed', __('Oklink reports payment cancelled.', 'oklink-woocommerce'));
+						$order->update_status('failed', __('Bihang reports payment cancelled.', 'bihang-woocommerce'));
 						break;
 
 				}
@@ -278,23 +278,23 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 		/**
 		 * Add this Gateway to WooCommerce
 		 **/
-		function woocommerce_add_oklink_gateway($methods) {
-			$methods[] = 'WC_Gateway_oklink';
+		function woocommerce_add_bihang_gateway($methods) {
+			$methods[] = 'WC_Gateway_bihang';
 			return $methods;
 		}
 
-		function woocommerce_handle_oklink_return() {
-			if (!isset($_GET['return_from_oklink']))
+		function woocommerce_handle_bihang_return() {
+			if (!isset($_GET['return_from_bihang']))
 				return;
 
 			if (isset($_GET['cancelled'])) {
 				$order = new WC_Order($_GET['order']['custom']);
 				if ($order->status != 'completed') {
-					$order->update_status('failed', __('Customer cancelled oklink payment', 'oklink-woocommerce'));
+					$order->update_status('failed', __('Customer cancelled bihang payment', 'bihang-woocommerce'));
 				}
 			}
 
-			// Oklink order param interferes with woocommerce
+			// bihang order param interferes with woocommerce
 			unset($_GET['order']);
 			unset($_REQUEST['order']);
 			if (isset($_GET['order_key'])) {
@@ -302,9 +302,9 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 			}
 		}
 
-		add_action('init', 'woocommerce_handle_oklink_return');
-		add_filter('woocommerce_payment_gateways', 'woocommerce_add_oklink_gateway');
+		add_action('init', 'woocommerce_handle_bihang_return');
+		add_filter('woocommerce_payment_gateways', 'woocommerce_add_bihang_gateway');
 	}
 
-	add_action('plugins_loaded', 'oklink_woocommerce_init', 0);
+	add_action('plugins_loaded', 'bihang_woocommerce_init', 0);
 }
